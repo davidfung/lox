962fdb76a145417d35703d85f3c8334d430cf834
davidfung <davidfung@amgcomputing.com>
2022-01-27 13:34:25 -0800

    Add Statements support

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -1,6 +1,8 @@
 package com.craftinginterpreters.lox;
 
-public class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
+
+public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   
   @Override
   public Object visitLiteralExpr(Expr.Literal expr) {
@@ -71,6 +73,23 @@ public class Interpreter implements Expr.Visitor<Object> {
     return expr.accept(this);
   }
 
+  private void execute(Stmt stmt) {
+    stmt.accept(this);
+  }
+
+  @Override
+  public Void visitExpressionStmt(Stmt.Expression stmt) {
+    evaluate(stmt.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitPrintStmt(Stmt.Print stmt) {
+    Object value = evaluate(stmt.expression);
+    System.out.println(stringify(value));
+    return null;
+  }
+
   @Override
   public Object visitBinaryExpr(Expr.Binary expr) {
     Object left = evaluate(expr.left);
@@ -116,10 +135,11 @@ public class Interpreter implements Expr.Visitor<Object> {
     return null;
   }
 
-  void interpret(Expr expression) { 
+  void interpret(List<Stmt> statements) {
     try {
-      Object value = evaluate(expression);
-      System.out.println(stringify(value));
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
     } catch (RuntimeError error) {
       Lox.runtimeError(error);
     }
