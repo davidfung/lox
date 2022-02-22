3529e82606c58f785ad6b653d1c98fb52fda9b80
davidfung <davidfung@amgcomputing.com>
2022-02-21 20:59:53 -0800

    Environments 8.3

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -6,7 +6,8 @@ import java.util.List;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 class Parser {
-  private static class ParseError extends RuntimeException {}
+  private static class ParseError extends RuntimeException {
+  }
 
   private final List<Token> tokens;
   private int current = 0;
@@ -18,18 +19,31 @@ class Parser {
   List<Stmt> parse() {
     List<Stmt> statements = new ArrayList<>();
     while (!isAtEnd()) {
-      statements.add(statement());
+      statements.add(declaration());
     }
 
-    return statements; 
+    return statements;
   }
 
   private Expr expression() {
     return equality();
   }
 
+  private Stmt declaration() {
+    try {
+      if (match(VAR))
+        return varDeclaration();
+
+      return statement();
+    } catch (ParseError error) {
+      synchronize();
+      return null;
+    }
+  }
+
   private Stmt statement() {
-    if (match(PRINT)) return printStatement();
+    if (match(PRINT))
+      return printStatement();
 
     return expressionStatement();
   }
@@ -40,12 +54,24 @@ class Parser {
     return new Stmt.Print(value);
   }
 
+  private Stmt varDeclaration() {
+    Token name = consume(IDENTIFIER, "Expect variable name.");
+
+    Expr initializer = null;
+    if (match(EQUAL)) {
+      initializer = expression();
+    }
+
+    consume(SEMICOLON, "Expect ';' after variable declaration.");
+    return new Stmt.Var(name, initializer);
+  }
+
   private Stmt expressionStatement() {
     Expr expr = expression();
     consume(SEMICOLON, "Expect ';' after expression.");
     return new Stmt.Expression(expr);
   }
-  
+
   private Expr equality() {
     Expr expr = comparison();
 
@@ -70,7 +96,7 @@ class Parser {
     return expr;
   }
 
-   private Expr term() {
+  private Expr term() {
     Expr expr = factor();
 
     while (match(MINUS, PLUS)) {
@@ -82,7 +108,7 @@ class Parser {
     return expr;
   }
 
-   private Expr factor() {
+  private Expr factor() {
     Expr expr = unary();
 
     while (match(SLASH, STAR)) {
@@ -105,12 +131,19 @@ class Parser {
   }
 
   private Expr primary() {
-    if (match(FALSE)) return new Expr.Literal(false);
-    if (match(TRUE)) return new Expr.Literal(true);
-    if (match(NIL)) return new Expr.Literal(null);
+    if (match(FALSE))
+      return new Expr.Literal(false);
+    if (match(TRUE))
+      return new Expr.Literal(true);
+    if (match(NIL))
+      return new Expr.Literal(null);
 
     if (match(NUMBER, STRING)) {
       return new Expr.Literal(previous().literal);
+    }
+
+    if (match(IDENTIFIER)) {
+      return new Expr.Variable(previous());
     }
 
     if (match(LEFT_PAREN)) {
@@ -118,13 +151,13 @@ class Parser {
       consume(RIGHT_PAREN, "Expect ')' after expression.");
       return new Expr.Grouping(expr);
     }
-  
+
     throw error(peek(), "Expect expression.");
   }
-  
-  // The match() method checks to see if the current token has any of the 
-  // given types.  If so, it consumes the token and returns true. Otherwise, 
-  // it returns false and leaves the current token alone. 
+
+  // The match() method checks to see if the current token has any of the
+  // given types. If so, it consumes the token and returns true. Otherwise,
+  // it returns false and leaves the current token alone.
   private boolean match(TokenType... types) {
     for (TokenType type : types) {
       if (check(type)) {
@@ -137,25 +170,28 @@ class Parser {
   }
 
   // The consume() method is similar to match() in that it checks to see if
-  // the next token is of the expected type. If so, it consumes the token 
+  // the next token is of the expected type. If so, it consumes the token
   // and everything is groovy. If some other token is there, then we’ve hit
   // an error.
   private Token consume(TokenType type, String message) {
-    if (check(type)) return advance();
+    if (check(type))
+      return advance();
 
     throw error(peek(), message);
   }
 
-  // The check() method returns true if the current token is of the given type. 
+  // The check() method returns true if the current token is of the given type.
   // Unlike match(), it never consumes the token, it only looks at it.
   private boolean check(TokenType type) {
-    if (isAtEnd()) return false;
+    if (isAtEnd())
+      return false;
     return peek().type == type;
   }
 
   // The advance() method consumes the current token and returns it.
   private Token advance() {
-    if (!isAtEnd()) current++;
+    if (!isAtEnd())
+      current++;
     return previous();
   }
 
@@ -189,7 +225,8 @@ class Parser {
     advance();
 
     while (!isAtEnd()) {
-      if (previous().type == SEMICOLON) return;
+      if (previous().type == SEMICOLON)
+        return;
 
       switch (peek().type) {
         case CLASS:
@@ -207,4 +244,3 @@ class Parser {
     }
   }
 }
-
